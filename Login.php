32e67b33df6c6d9c724a00f5904563afa563482e
root <root@ip-172-31-23-94.eu-west-1.compute.internal>
2017-04-13 11:46:58 +0000

    Preparing to implement new message.

--- Login.php
+++ Login.php
@@ -3,27 +3,29 @@
     $db = new DatabaseController();
     $connection = $db -> connect();
 
-    if(isset($_POST['phoneNumber']) && isset($_POST["password"])) {
+    if(isset($_POST['PhoneNumber']) && isset($_POST['Password'])) {
 
-      $phoneNumber = $db -> quote($_POST['phoneNumber']);
-      $password = $db->quote($_POST["password"]);
+      $phoneNumber = $db->quote($_POST['PhoneNumber']);
+      $password = $db->quote($_POST['Password']);
 
       $sqlSelect = "SELECT * FROM User WHERE PhoneNumber = '$phoneNumber';";
-      
-      $result = $db -> select($sqlSelect);
+
+      $result = $db->select($sqlSelect);
       $colPassword = $result[0]['Password'];
       $response = array();
       $response["success"] = false;
+
       if(password_verify($password, $colPassword)) {
         $response["success"] = true;
         $response["UserID"] = $result[0]["UserID"];
         echo  json_encode($response, JSON_FORCE_OBJECT);
       }
       else{
-  	     echo "{'success': false}";
+             echo "{'success': false}";
       }
     }
     else{
       echo "{'success': false}";
     }
 ?>
+
