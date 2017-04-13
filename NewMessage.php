b1993ba01a9d15637dd24eeb76dea671dee1a2b7
RoryEgan <roryegan1997@gmail.com>
2017-04-13 15:31:59 +0100

    Updated messaging script.

--- NewMessage.php
+++ NewMessage.php
@@ -8,14 +8,34 @@ if($connection) {
   $threadID = $db -> quote($_POST["ThreadID"]);
   $senderID = $db -> quote($_POST["SenderID"]);
   $content = $db -> quote($_POST["Content"]);
+  $targetPhoneNumber = $db -> quote($_POST["TargetNumber"]);
+  $targetID = "";
+
+  function getTargetID() {
+
+    global $targetPhoneNumber, $targetID;
+
+
+    $sql = "SELECT UserID
+            FROM User
+            WHERE PhoneNumber = '$targetPhoneNumber'";
+
+    $targetID = $db -> query($sql);
+    if($targetID) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 
   function sendMessage() {
 
     global $threadID, $senderID, $content, $db;
 
     $sql = "INSERT INTO Message
-    (MessageID, ThreadID, SenderID, Content)
-    VALUES ('0', '$threadID', '$senderID', '$content');";
+    (MessageID, ThreadID, SenderID, TargetID, Content)
+    VALUES ('0', '$threadID', '$senderID', '$targetID', '$content');";
 
     $result = $db -> query($sql);
     if($result) {
@@ -30,7 +50,7 @@ if($connection) {
   $response["success"] = false;
 
 
-  if(newMessage()) {
+  if(sendMessage()) {
     $response["success"] = true;
     $response["UserID"] = $db -> getLastInsertID();
   }
