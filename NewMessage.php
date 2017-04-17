43f52079abc17faef4fdd446709c14dbcf06a7a9
RoryEgan <roryegan1997@gmail.com>
2017-04-17 13:13:42 +0100

    Created new functions in the NewMessage class.

--- NewMessage.php
+++ NewMessage.php
@@ -5,64 +5,61 @@ $db = new DatabaseController();
 $connection = $db -> connect();
 
 if($connection) {
-  
+
   $threadID = $db -> quote($_POST["ThreadID"]);
   $senderID = $db -> quote($_POST["SenderID"]);
   $content = $db -> quote($_POST["Content"]);
   $targetPhoneNumber = $db -> quote($_POST["TargetNumber"]);
   $targetID = "0";
-  
+
   function getTargetID() {
-  
+
     global $targetPhoneNumber, $targetID,  $db;
-  
+
     $sql = "SELECT UserID
     FROM User
     WHERE PhoneNumber = '$targetPhoneNumber';";
     $returned = $db -> select($sql);
     $targetID = $returned[0]['UserID'];
-  
+
     return $targetID;
   }
-  
+
   $targetID = getTargetID();
-  
+
   function sendMessage() {
-  
+
     global $threadID, $senderID, $targetID, $content, $db;
-  
+
     $sql = "INSERT INTO Message
     (MessageID, ThreadID, SenderID, TargetID, Content)
     VALUES ('0', '$threadID', '$senderID', '$targetID', '$content');";
     $result = $db -> query($sql);
-  
+
     if($result) {
       return true;
     }
-  
+
     else {
       return false;
-  
+
     }
   }
 
-  function newThread() {
+  function insertNewThread() {
 
     global $senderID, $targetID, $db;
-    $query = "SELECT *
-    FROM Message
-    WHERE SenderID = '$senderID' AND TargetID = '$targetID';";
-    $result = $db -> select($query);
 
-    if(mysqli_num_rows($result[0]['MessageID']) < 1) {
+    $insertion = "INSERT INTO Thread
+    (ThreadID, UserOne, UserTwo)
+    VALUES ('0', '$senderID', '$targetID');";
+    $db -> query($insertion);
+  }
 
-     $insertion = "INSERT INTO Thread
-     (ThreadID, UserOne, UserTwo)
-     VALUES ('0', '$senderID', '$targetID');";
-      $db -> query($insertion);
- 
-   }
-    
+  function updateMessageThreadID() {
+
+    global $senderID, $targetID, $db;
+
     $updateQuery = "UPDATE Message
 	         SET ThreadID = (SELECT ThreadID
 		 FROM Thread
@@ -70,17 +67,44 @@ if($connection) {
 		 AND Thread.UserTwo = Message.TargetID);";
 
     $db -> query($updateQuery);
-  
+  }
+
+  function newThread() {
+
+    global $senderID, $targetID, $db;
+
+    $query1 = "SELECT MessageID
+    FROM Message
+    WHERE SenderID = '$senderID' AND TargetID = '$targetID';";
+    $result1 = $db -> select($query1);
+
+    $query2 = "SELECT MessageID
+    FROM Message
+    WHERE SenderID = '$targetID' AND TargetID = '$senderID';";
+    $result2 = $db -> select($query2);
+
+    if(mysqli_num_rows($result1) == 0 || mysqli_num_rows($result2) == 0) {
+
+     insertNewThread();
+
+   }
+   else {
+
+   }
+
+   updateMessageThreadID();
+
+
   }
 
   $response = array();
   $response["success"] = false;
-  
+
   if(sendMessage()) {
     newThread();
     $response["success"] = true;
     $response["UserID"] = $db -> getLastInsertID();
-  
+
   }
   echo json_encode($response);
 }
